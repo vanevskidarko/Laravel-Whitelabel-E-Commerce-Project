a10a5d113e12fe0b3cd95834d7e28433b1f97818
vanevskidarko <78539262+vanevskidarko@users.noreply.github.com>
2022-02-10 12:29:47 +0100

    Update 2022_02_10_112719_create_categories_table.php

--- database/migrations/2022_02_10_112719_create_categories_table.php
+++ database/migrations/2022_02_10_112719_create_categories_table.php
@@ -15,6 +15,14 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->longText('description');
+            $table->tinyInteger('status')->default('0');
+            $table->tinyInteger('popular')->default('0');
+            $table->string('meta_title');
+            $table->string('meta_descrip');
+            $table->string('meta_keywords');
             $table->timestamps();
         });
     }
